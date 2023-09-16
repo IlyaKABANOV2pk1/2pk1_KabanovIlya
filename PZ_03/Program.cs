1d1e00285b2f47924d75d6a6f45f9277496f1fb6
Илья Кабанов <tolatolkin@gmail.com>
2023-09-16 13:12:49 +0500

    добавлена 3 практика

---
+++ PZ_03/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace PZ_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = Convert.ToInt32(Console.ReadLine());
+            double ag;
+
+            if (age % 10 == 1)
+                ag = 1;
+
+
+            else if (1 < age && age < 5)
+                ag = 2;
+            else if (age > 4 && age < 21)
+                ag = 3;
+            else if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4)
+                ag = 4;
+            else
+                ag = 5;
+            switch (ag)
+            {
+                case 1:
+                    Console.Write("возраст -" + age + "год");
+                    break;
+                case 2:
+                    Console.Write("возраст -" + age + "года");
+                    break;
+                case 3:
+                    Console.Write("возраст " + age + "лет");
+                    break;
+                case 4:
+                    goto case 2;
+                case 5:
+                    goto case 3;
+            }
+
+
+            
+        }
+    }
+}
