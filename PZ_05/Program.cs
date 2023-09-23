f8fcb800ba37834ddfb8abf56ed0b50930420590
Илья Кабанов <tolatolkin@gmail.com>
2023-09-23 16:29:07 +0500

    добавлена 5 практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое положительное число n:");
+            int n = int.Parse(Console.ReadLine());
+
+            int m = n % 2 == 0 ? n + 1 : n;
+
+            int sum = 0;
+            int num = m;
+
+            while (num <= m * 2)
+            {
+                sum += num;
+                num += 2;
+            }
+
+            Console.WriteLine($"Сумма всех нечетных чисел диапазона от {m} до {m*2} равна {sum}");
+        }
+    }
+}
