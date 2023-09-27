2db31c493057bab60a6a49631426174212b2f403
Илья Кабанов <tolatolkin@gmail.com>
2023-09-27 17:15:51 +0500

    добавлена 6 практика

---
+++ PZ_06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PZ_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[49];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i + 1;
+            }
+
+            // Удаляем элементы меньше 15
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 16)
+                {
+                    array[i] = 0;
+                }
+                Console.Write(array[i] + " ");
+            }
+        }
+        }
+    }
