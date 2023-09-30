afe353f6d79b97f197bdc25ba8bd93fd68835016
IlyaKABANOV2pk1 <tolatolkin@gmail.com>
2023-09-30 08:26:42 +0300

    добавлена 7 практика

---
+++ PZ_07/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PZ_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5; 
+            int[,] array = new int[n, n]; 
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = Math.Abs(i - j) + 1;
+                }
+            }
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
