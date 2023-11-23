d1ccb4a006cc7b7bd7e3eb4f38e263e5cfbf284f
Илья Кабанов <tolatolkin@gmail.com>
2023-11-23 20:03:09 +0500

    добавлена 10 практика

---
+++ PZ_10/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "cтрока для переворота"; //не знаю как с любой вводимой строкой
+            string[] words = str.Split(' ');//делим строку на слова
+            string newStr = "";
+            foreach (var word in words)  //проверяем каждое слово
+            {
+                for (int i = word.Length; i != 0; i--) //каждое слово с обратной стороны
+                {
+                    newStr += word[i - 1];  // запись перевернутого слова в новую строку
+                }
+                newStr += " ";
+               
+            }
+            Console.WriteLine(newStr);
+        }
+    }
+}
